11eb691a419e7dd6fbc4f608f17859c5b3d1c870
Lisa Ma <limawebdev@gmail.com>
2016-09-23 19:15:50 -0600

    knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
 'use strict';
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection:'postgres://localhost/bookshelf_dev'
-  },
-  test: {
-    client:'pg',
-    connection:'postgres://localhost/bookshelf_dev'
-  }
+ development: {
+  client: 'pg',
+  connection: 'postgres://localhost/bookshelf_dev'
+ },
+ test: {
+  client: 'pg',
+  connection: 'postgres://localhost/bookshelf_dev'
+ },
+ production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+ }
 };
